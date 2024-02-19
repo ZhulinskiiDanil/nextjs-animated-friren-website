0e8b41a5b7494a7f887f902ab0096fea9f5c8064
ZhulinskiiDanil <zhulinskiidanil@gmail.com>
2024-02-19 03:14:24 +0100

    chore: added friren person to landing

---
+++ src/app/components/Landing/Friren/index.tsx
@@ -0,0 +1,70 @@
+'use client';
+import clsx from 'clsx'
+import gsap from 'gsap';
+import styles from './index.module.scss'
+
+import type { CSSProperties } from 'react';
+
+// Hooks
+import { useGSAP } from '@gsap/react';
+import { useMouse, useWindowSize } from '@uidotdev/usehooks';
+
+// Components
+import Image from 'next/image'
+
+export function Friren() {
+  const [mouse, ref] = useMouse<HTMLDivElement>();
+  const windowSize = useWindowSize();
+
+  useGSAP(() => {
+    gsap.fromTo(ref.current, {
+      scale: .95,
+      marginTop: 48,
+      opacity: 0,
+    }, {
+      scale: 1,
+      marginTop: 0,
+      opacity: 1,
+      duration: 2,
+      delay: 1
+    })
+  })
+
+  return <div
+    ref={ref}
+    className={styles.person}
+    style={{
+      '--x': mouse.x / (windowSize.width || 1),
+      '--y': mouse.y / (windowSize.height || 1)
+    } as CSSProperties & {
+      '--x': number, '--y': number,
+    }}
+  >
+    <div className={styles.wrapper}>
+      <div className={clsx(styles.sprite, styles.leftEye)}>
+        <Image
+          src="/sprites/friren/left-eye.png"
+          alt="Sprite"
+          priority
+          fill
+        />
+      </div>
+      <div className={clsx(styles.sprite, styles.rightEye)}>
+        <Image
+          src="/sprites/friren/right-eye.png"
+          alt="Sprite"
+          priority
+          fill
+        />
+      </div>
+      <div className={clsx(styles.sprite, styles.main)}>
+        <Image
+          src="/sprites/friren/main.png"
+          alt="Sprite"
+          priority
+          fill
+        />
+      </div>
+    </div>
+  </div>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import styles from "./page.module.scss";
 import { Decor } from "./components/Landing/Decor";
 import { AnimeName } from "./components/Landing/AnimeName";
 import { BackgroundIllustration } from "@/app/components/Landing/BackgroundIllustration";
+import { Friren } from "./components/Landing/Friren";
 
 export default function Landing() {
   return (
     <main className={styles.main}>
       <Decor />
+      <Friren />
       <AnimeName />
       <BackgroundIllustration />
     </main>
